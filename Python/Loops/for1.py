c5ecdb3723b0a1a058680bc217e0b7a927d762ed
Abhishek Bhat <bhatabhishek409@gmail.com>
2025-11-02 23:08:19 +0530

    Add files via upload

---
+++ Python/Loops/for1.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the value of x "))
+for i in range(x):
+    print(i)
+if x>10:
+    print("X is greater than 10")
+elif x==10:
+    print("X is equal to 10")
+else:
+    print("X is less than 10")
---
+++ Python/Loops/loop1.py
@@ -0,0 +1,3 @@
+sub= input("Enter the subjects you have ").split()
+for i in range(len(sub)):
+    print(i,sub[i])
